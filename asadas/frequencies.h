5ec205b9b8c9c9f41bf25d2e5e19c5d42d5ecd4f
anthonyarguedas <anthony_arguedas@outlook.com>
2024-02-26 00:52:11 -0600

    Add LoRa logging for Octave and height sensors

---
+++ asadas/frequencies.h
@@ -0,0 +1,8 @@
+// Frequency for sending Modbus requests to the slave
+#define MODBUS_POLLING_FREQ_MS 1000 // ms
+
+// Height sensor polling frequency
+#define HEIGHT_POLLING_FREQ_MS 1000
+
+// LoRa sender frequency
+#define LORA_SENDER_FREQ_MS 10000
