ac35e352ea95895c8d1f4b590bb6abf2032201b8
anthonyarguedas <anthony_arguedas@outlook.com>
2024-02-26 01:14:40 -0600

    Move Modbus library to main folder

--- asadas/asadas.ino
+++ asadas/asadas.ino
@@ -51,7 +51,7 @@ class AverageCalculator {
         int32avg = sum / (counter * 1.0);
       }
     }
-}
+};
 
 // Define the OctaveModbusWrapper object, using the RS-485 port for Modbus and Serial0 for logging
 OctaveModbusWrapper octave(RS485);
@@ -118,13 +118,13 @@ void loop() {
         // Read Signed Current Flow from Octave meter via Modbus
         octave.SignedCurrentFlow(64);
         // Multiply by 100 to preserve two decimal places, then truncate to 16 bits
-        int16_t truncatedSignedCurrentFlow = doubleBuffer * 100;
+        int16_t truncatedSignedCurrentFlow = octave.doubleBuffer * 100;
         SignedCurrentFlowArr.append(truncatedSignedCurrentFlow);
 
         // Read Signed Current Flow from Octave meter via Modbus
         octave.NetSignedVolume(64);
         // Multiply by 100 to preserve two decimal places, then truncate to 32 bits
-        int32_t truncatedNetSignedVolume = doubleBuffer * 100;
+        int32_t truncatedNetSignedVolume = octave.doubleBuffer * 100;
         NetSignedVolumeArr.append(truncatedNetSignedVolume);
 
         // Restart time counter
@@ -220,4 +220,81 @@ int i = 0;
 
   //Se envía la cadena de datos por LoRa
   send(DatosExtendidos, 100);
+}
+
+void pedidoUtil()  {
+  int i=0;
+  byte DatosUtiles[13];
+  byte DatosGeneral[36];//32
+  byte DatosCeldas[15];//10
+  byte PedidoGeneral[7] = {0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77};
+  byte PedidoCeldas[7] = {0xDD, 0xA5, 0x04, 0x00, 0xFF, 0xFC, 0x77};
+
+  Serial.println("#### LECTURA DE DATOS ÚTILES ####");
+
+  delay(100); // Add a small delay to allow the data to be sent
+  Serial1.flush(); // Clear the buffer to ensure we read only the response
+  Serial1.write(PedidoGeneral, sizeof(PedidoGeneral));
+  delay(100); // Add a small delay to allow the data to be sent
+  Serial1.flush(); // Clear the buffer to ensure we read only the response
+
+  //Serial.write(PedidoGeneral, sizeof(PedidoGeneral));
+  if(Serial1.available() > 0){
+    Serial1.readBytesUntil(0xFF, DatosGeneral, 36);
+    Serial.print("Datos Generales: ");
+    for(i=0; i<sizeof(DatosGeneral); i++){
+      if(DatosGeneral[i] < 16) Serial.print("0");
+      Serial.print(DatosGeneral[i],HEX);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
+  }
+
+  delay(100); // Add a small delay to allow the data to be sent
+  Serial1.flush(); // Clear the buffer to ensure we read only the response
+  Serial1.write(PedidoCeldas, sizeof(PedidoCeldas));
+  delay(100); // Add a small delay to allow the data to be sent
+  Serial1.flush(); // Clear the buffer to ensure we read only the response
+
+  //Serial.write(PedidoCeldas, sizeof(PedidoCeldas));
+  if(Serial1.available() > 0){
+    Serial1.readBytesUntil(0xFF, DatosCeldas, 15);
+    Serial.print("Datos de Celda: ");
+    for(i=0; i<sizeof(DatosCeldas); i++){
+      if(DatosCeldas[i] < 16) Serial.print("0");
+      Serial.print(DatosCeldas[i],HEX);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
+  }
+
+
+  Serial.print("Datos seleccionados para transmisión: ");
+  //Voltaje Actual
+  DatosUtiles[0]=DatosGeneral[4];
+  DatosUtiles[1]=DatosGeneral[5];
+  //Corriente Actual
+  DatosUtiles[2]=DatosGeneral[6];
+  DatosUtiles[3]=DatosGeneral[7];
+  //Porcentaje Restante
+  DatosUtiles[4]=DatosGeneral[23];
+  //Voltaje Celda 1
+  DatosUtiles[5]=DatosCeldas[4];
+  DatosUtiles[6]=DatosCeldas[5];
+  //Voltaje Celda 2
+  DatosUtiles[7]=DatosCeldas[6];
+  DatosUtiles[8]=DatosCeldas[7];
+  //Voltaje Celda 3
+  DatosUtiles[9]=DatosCeldas[8];
+  DatosUtiles[10]=DatosCeldas[9];
+  //Voltaje Celda 4
+  DatosUtiles[11]=DatosCeldas[10];
+  DatosUtiles[12]=DatosCeldas[11];
+
+  for(i=0; i<sizeof(DatosUtiles); i++){
+      if(DatosUtiles[i] < 16) Serial.print("0");
+      Serial.print(DatosUtiles[i],HEX);
+      Serial.print(" ");
+    }
+  Serial.println(" ");
 }
