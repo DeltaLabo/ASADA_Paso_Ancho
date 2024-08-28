fadbc8408befbdaac7069ca6704a963954b529bc
anthonyarguedas <anthony_arguedas@outlook.com>
2024-08-28 10:19:34 -0600

    Update pins.h

--- main/pins.h
+++ main/pins.h
@@ -1,8 +1,13 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define RS485_TX_PIN D6
-#define RS485_RX_PIN D7
-#define RS485_RTS_PIN D5
+#define HEIGHT_SENSOR_PIN A0
+
+#define RS485_RTS_PIN D1
+#define RS485_TX_PIN D2
+#define RS485_RX_PIN D3
+
+#define LTE_TX_PIN D4
+#define LTE_RX_PIN D5
 
 #endif
