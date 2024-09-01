7bf2861fecbb5ad292da2a8091ec21da69b975df
anthonyarguedas <anthony_arguedas@outlook.com>
2024-08-31 18:00:50 -0600

    Remove references to LoRa

--- main/frequencies.h
+++ main/frequencies.h
@@ -2,7 +2,7 @@
 #define MODBUS_POLLING_FREQ_MS 1000 // ms
 
 // Height sensor polling frequency
-#define HEIGHT_POLLING_FREQ_MS 1000
+#define HEIGHT_POLLING_FREQ_MS 1000 // ms
 
-// LoRa sender frequency
-#define LORA_SENDER_FREQ_MS 10000
+// Logging frequency
+#define LOGGING_FREQ_MS 10000 // ms
